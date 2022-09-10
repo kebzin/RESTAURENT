aeabaeb361ced8abe4795aad2585c2e71a83b8d5
kebzin <honorablewaiga@gmail.com>
2022-09-10 05:44:58 -0700

    connecting to a server state 1

--- src/index.js
+++ src/index.js
@@ -1,56 +1,63 @@
-const express = require('express')
-const app = express()
-const port = 3001
+require("dotenv").config();
+const mangoose = require('mongoose');
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
 
-const middleware =require('./utilities/midleware')
+const middleware = require("./utilities/midleware");
+const { default: mongoose } = require("mongoose");
 
-app.use(express.json())
-app.use(middleware.RequestLogger)
+app.use(express.json());
+app.use(middleware.RequestLogger);
+const URI = process.env.MONGODB_URI;
+try {
+  mongoose.connect(URI)
+  console.log('connected');
+} catch (error) {
+  console.log(error);
+}
 
 
 
-let me ={
-    name: "kebba",
-    last: "waiga",
-    Status: "online"
+
+let me = {
+  name: "kebba",
+  last: "waiga",
+  Status: "online",
 };
 
 let users = [
   {
     id: 1,
-    userName: 'kebba waiga',
-    phone: '2493268',
-    password: 'Howareyoudoing1',
-    Status: true
-
+    userName: "kebba waiga",
+    phone: "2493268",
+    password: "Howareyoudoing1",
+    Status: true,
   },
-  { 
-   id: 2,
-    userName: 'buba camara',
-    phone: '2493263',
-    password: '12345',
-    status: false
-
-  }
+  {
+    id: 2,
+    userName: "buba camara",
+    phone: "2493263",
+    password: "12345",
+    status: false,
+  },
 ];
 
-
-app.get('/', (req, res) => {
-  res.send('Hellow World!')
+app.get("/", (req, res) => {
+  res.send("Hellow World!");
 });
-app.get('/me',(req, res) =>{
-    res.json(me)
-} );
- 
-app.get('/users/:id', (req, res) =>{
+app.get("/me", (req, res) => {
+  res.json(me);
+});
+
+app.get("/users/:id", (req, res) => {
   const id = req.params.id;
-  const user = users.find((user) => user.id ===Number(id))
-  
+  const user = users.find((user) => user.id === Number(id));
+
   if (user) {
-    res.json(user)
-  }
-  else{
-    res.status(404).end()
+    res.json(user);
+  } else {
+    res.status(404).end();
   }
 });
 
@@ -59,23 +66,22 @@ app.get('/users/:id', (req, res) =>{
 //    users = users.filter((user) => user.id !== Number(id))
 // })
 
-app.delete('/users/:id', (req , res) =>{
+app.delete("/users/:id", (req, res) => {
   const id = req.params.id;
-  users = users.filter((user) =>(user.id !==id))
+  users = users.filter((user) => user.id !== id);
 });
 
-app.get('/users', (req, res)=>{
+app.get("/users", (req, res) => {
   res.json(users);
-})
+});
 
-app.post('/users', (req, res) => {
-  const container = req.body
-  res.json(container)
+app.post("/users", (req, res) => {
+  const container = req.body;
+  res.json(container);
   console.log(container);
-})
-
+});
 
 // app.use(middleware.unknownExport)
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
